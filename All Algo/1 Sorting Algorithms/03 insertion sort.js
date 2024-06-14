dc28dba949efc28d696bab2e1c7ee38706775910
Inam <inamm081@gmail.com>
2024-06-14 10:38:54 -0700

    Breaking Down Insertion Sort Algorithm

--- All Algo/1 Sorting Algorithms/03 insertion sort.js
+++ All Algo/1 Sorting Algorithms/03 insertion sort.js
@@ -1,10 +1,10 @@
 function insertionSort(arr) {
     let len = arr.length;
     for (let i = 1; i < len; i++) { // 1,2,3,...
-        let key = arr[i]; // 12, 11
-        let j = i - 1; // 0,1,2,3,...
-        while (j >= 0 && arr[j] > key) {// 12 > 11 
-            arr[j + 1] = arr[j];
+        let key = arr[i]; // 11
+        let j = i - 1; // Select Dynamic range 0|1|2 To Compare Previous Element
+        while (j >= 0 && arr[j] > key) {// 12 > 11  || Reverse loop
+            arr[j + 1] = arr[j]; // It is replacing next element with previous element 
             j = j - 1;
         }
         arr[j + 1] = key;
